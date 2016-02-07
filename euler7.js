e839ab0d494b42cb91c9a274474116d6953073e5
jrisk <joeyrsk@gmail.com>
2016-02-07 02:03:34 -0600

    7 more primes algos

---
+++ euler7.js
@@ -0,0 +1,17 @@
+//Euler 7: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13
+//we can see that the 6th prime is 13.
+
+//What is the 10,001st prime number?
+
+function findPrime(n) {
+	
+	for (i = 1; i < 100; i++) {
+
+		if (number % i == 0) {
+			//not Prime number;
+		}
+
+		else {
+		}
+	}
+}
